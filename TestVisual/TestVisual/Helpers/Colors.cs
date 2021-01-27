f07362c6ffd07e306ffbee180e82603a3e373c7f
Giovanni Santarelli <giovanni.santarelli@outlook.it>
2021-01-27 16:16:30 +0100

    Aggiungere i file di progetto.

---
+++ TestVisual/TestVisual/Helpers/Colors.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace TestVisual.Helpers
+{
+    static class Colors
+    {
+        public static Color AppGreen = Color.FromHex("#7CB342");
+        public static Color AppRed = Color.FromHex("#F4511E");
+        public static Color AppWhite = Color.FromHex("#FFFFFF");
+        public static Color AppLightGrey = Color.FromHex("#F1F1F1");
+        public static Color AppYellow = Color.FromHex("#FFFF00");
+        public static Color BgColor = Color.FromHex("#F9F9F9");
+        public static Color BarBgColor = Color.FromHex("#607D8B");
+        public static Color BarTextColor = Color.FromHex("#FFFFFF");
+        public static Color LoginTextColor = Color.FromHex("#607D8B");
+        public static Color ButtonBgColor = Color.FromHex("#FFA000");
+        public static Color ButtonTextColor = Color.FromHex("#FFFFFF");
+        public static Color ButtonBorderColor = Color.FromHex("#607D8B");
+        public static Color ActivityBgColor = Color.FromHex("#000000");
+        public static Color ActivityTextColor = Color.FromHex("#FFFFFF");
+        public static Color EntryDisabledBgColor = Color.FromHex("#D3D3D3");
+        public static Color ListTextColor = Color.FromHex("#344955");
+        public static Color ListDetailColor = Color.FromHex("#90A4AE");
+        public static Color ListSeparatorColor = Color.FromHex("#FFA000");
+        public static Color ListImageColor = Color.FromHex("#FFB74D");
+    }
+}
---
+++ TestVisual/TestVisual/Helpers/ContainerIconFont.cs
@@ -0,0 +1,23 @@
+﻿namespace TestVisual.Helpers
+{
+    static class ContainerIconFont
+    {
+        public const string Barcode = "\U000f0071";
+        public const string BarcodeScan = "\U000f0072";
+        public const string Basket = "\U000f0076";
+        public const string Cancel = "\U000f073a";
+        public const string Cart = "\U000f0110";
+        public const string CheckBoxOutline = "\U000f0c52";
+        public const string FeatureSearch = "\U000f0a49";
+        public const string FileDocument = "\U000f0219";
+        public const string DocumentToCheck = "\U000f0216";
+        public const string File = "\U000f0214";
+        public const string Flash = "\U000f0241";
+        public const string Login = "\U000f0342";
+        public const string MenuRight = "\U000f035f";
+        public const string Qrcode = "\U000f0432";
+        public const string QrcodeScan = "\U000f0433";
+        public const string Shopping = "\U000f049a";
+        public const string Store = "\U000f04dc";
+    }
+}
